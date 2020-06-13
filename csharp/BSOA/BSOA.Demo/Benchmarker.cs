bab9902d3c35ef3bd9887f69e56b5da70eb2c297
Scott Louvau <sclouvau@microsoft.com>
2020-06-12 17:32:11 -0700

    Fix bad demo change

--- csharp/BSOA/BSOA.Demo/Benchmarker.cs
+++ csharp/BSOA/BSOA.Demo/Benchmarker.cs
@@ -39,7 +39,7 @@ namespace BSOA.Demo
             SarifLog bsoa = null, unused = null;
 
             // Convert SarifLog to JSON, SoA JSON, and SoA Binary forms
-            //Convert(forceReconvert);
+            Convert(forceReconvert);
 
             //// Load with diagnostics (see column sizes)
             //Console.WriteLine();
