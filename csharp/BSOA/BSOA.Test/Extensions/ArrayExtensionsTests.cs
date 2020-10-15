75eb0803fd22b0e2e4fa3bb4295743fe5d79d5b3
Scott Louvau <sclouvau@microsoft.com>
2020-10-15 13:54:19 -0700

    Add ArrayExtensions.BinarySearch.

---
+++ csharp/BSOA/BSOA.Test/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿
+using BSOA.Extensions;
+
+using System;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace BSOA.Test.Extensions
+{
+    public class ArrayExtensionsTests
+    {
+        [Fact]
+        public void ArrayExtensions_BinarySearch()
+        {
+            string[] values = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+
+            int[] indices = new int[]
+            {
+                5, 4, 1,                    // First sorted set (0 - 2)
+                8, 7, 6, 10, 0,             // Second sorted set (3 - 7)
+                2,                          // Third sorted set (8)
+                4, 1, 3, 2,                 // Fourth sorted set (9 - 12)
+            };
+
+            IComparer<string> comparer = (IComparer<string>)StringComparer.Ordinal;
+
+            // Find 'four' in first set (middle value)
+            Assert.Equal(1, ArrayExtensions.IndirectBinarySearch(indices, 0, 3, values, "four", comparer));
+
+
+            // Find 'eight' in second set (first value)
+            Assert.Equal(3, ArrayExtensions.IndirectBinarySearch(indices, 3, 5, values, "eight", comparer));
+
+            // Find 'seven' in second set (second value)
+            Assert.Equal(4, ArrayExtensions.IndirectBinarySearch(indices, 3, 5, values, "seven", comparer));
+
+            // Find 'zero' in second set (last value)
+            Assert.Equal(7, ArrayExtensions.IndirectBinarySearch(indices, 3, 5, values, "zero", comparer));
+
+            // Find 'ten' in second set (second to last value)
+            Assert.Equal(6, ArrayExtensions.IndirectBinarySearch(indices, 3, 5, values, "ten", comparer));
+
+            // Find 'two' in second set (missing; would be before 'zero' and after 'ten')
+            Assert.Equal(~7, ArrayExtensions.IndirectBinarySearch(indices, 3, 5, values, "two", comparer));
+
+
+            // Find 'two' in third set (only value)
+            Assert.Equal(8, ArrayExtensions.IndirectBinarySearch(indices, 8, 1, values, "two", comparer));
+
+            // Find 'three' in third set (missing, before only value)
+            Assert.Equal(~8, ArrayExtensions.IndirectBinarySearch(indices, 8, 1, values, "three", comparer));
+
+            // Find 'zero' in third set (missing, after only value
+            Assert.Equal(~9, ArrayExtensions.IndirectBinarySearch(indices, 8, 1, values, "zero", comparer));
+
+
+            // Find 'two' in an empty set (not found; insert at start index)
+            Assert.Equal(~8, ArrayExtensions.IndirectBinarySearch(indices, 8, 0, values, "two", comparer));
+
+
+            // Find 'four' in fourth set (first; even count)
+            Assert.Equal(9, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "four", comparer));
+
+            // Find 'one' in fourth set (second; even count)
+            Assert.Equal(10, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "one", comparer));
+
+            // Find 'three' in fourth set (third; even count)
+            Assert.Equal(11, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "three", comparer));
+
+            // Find 'two' in fourth set (fourth; even count)
+            Assert.Equal(12, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "two", comparer));
+
+
+            // Find 'five' in fourth set (before first; even count)
+            Assert.Equal(~9, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "five", comparer));
+
+            // Find 'fours' in fourth set (after first; even count)
+            Assert.Equal(~10, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "fours", comparer));
+
+            // Find 'ones' in fourth set (after second; even count)
+            Assert.Equal(~11, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "ones", comparer));
+
+            // Find 'threes' in fourth set (after third; even count)
+            Assert.Equal(~12, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "threes", comparer));
+
+            // Find 'zero' in fourth set (after last; even count)
+            Assert.Equal(~13, ArrayExtensions.IndirectBinarySearch(indices, 9, 4, values, "zero", comparer));
+        }
+    }
+}
--- csharp/BSOA/BSOA/Extensions/ArrayExtensions.cs
+++ csharp/BSOA/BSOA/Extensions/ArrayExtensions.cs
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using BSOA.Model;
+
 using System;
+using System.Collections.Generic;
 
 namespace BSOA.Extensions
 {
@@ -12,7 +15,7 @@ namespace BSOA.Extensions
         public static void ResizeTo<T>(ref T[] array, int neededSize, T defaultValue = default(T), int minSize = MinimumSize)
         {
             int currentLength = (array?.Length ?? 0);
-            
+
             // Allocate new array (at least 50% growth)
             int newLength = Math.Max(minSize, Math.Max(neededSize, (currentLength + currentLength / 2)));
             T[] newArray = new T[newLength];
@@ -33,6 +36,53 @@ namespace BSOA.Extensions
             }
 
             array = newArray;
+        }
+
+        /// <summary>
+        ///  Binary Search for a particular key, where there is an array of keys
+        ///  but we're searching a range of an indices array pointing to those keys.
+        /// </summary>
+        /// <remarks>
+        ///  See GenericArraySortHelper.BinarySearch at https://referencesource.microsoft.com; implementation closely matched.
+        ///  Added because .NET has Sort() with an indirection array, but not BinarySearch().
+        /// </remarks>
+        /// <typeparam name="T">Type of values being searched</typeparam>
+        /// <param name="indices">Array containing indices to values to search</param>
+        /// <param name="index">Index in indices array of first position to search</param>
+        /// <param name="length">Number of elements in indices array to check</param>
+        /// <param name="values">Array containing values pointed to by indices</param>
+        /// <param name="value">Value to find</param>
+        /// <returns>Index where values[indices[index]] == value, or bitwise complement of first index larger than value.</returns>
+        public static int IndirectBinarySearch<T>(int[] indices, int index, int length, IReadOnlyList<T> values, T value, IComparer<T> comparer)
+        {
+            int lo = index;
+            int hi = index + length - 1;
+            while (lo <= hi)
+            {
+                int mid = (lo + ((hi - lo) >> 1));
+                int valueIndex = indices[mid];
+                T midValue = values[valueIndex];
+
+                int cmp = comparer.Compare(midValue, value);
+                if (cmp == 0)
+                {
+                    return mid;
+                }
+
+                if (cmp < 0)
+                {
+                    // value > midValue
+                    lo = mid + 1;
+                }
+                else
+                {
+                    // value < midValue
+                    hi = mid - 1;
+                }
+            }
+
+            // Value not found
+            return ~lo;
         }
     }
 }
