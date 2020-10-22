96882bc35b94c3140c772912b3d36b1450795892
Scott Louvau <sclouvau@microsoft.com>
2020-10-22 13:52:57 -0700

    NullableColumn: Defer initializting IsNull.

--- csharp/BSOA/BSOA/Column/NullableColumn.cs
+++ csharp/BSOA/BSOA/Column/NullableColumn.cs
@@ -17,12 +17,14 @@ namespace BSOA.Column
     /// <typeparam name="T"></typeparam>
     public class NullableColumn<T> : LimitedList<T>, IColumn<T> where T : class
     {
+        protected bool NullByDefault;
         protected BooleanColumn IsNull;
         internal IColumn<T> Values;
 
         protected NullableColumn(IColumn<T> values, bool nullByDefault)
         {
-            IsNull = new BooleanColumn(nullByDefault);
+            // Defer IsNull initialization until needed to save memory.
+            NullByDefault = nullByDefault;
             Values = values;
         }
 
@@ -41,17 +43,26 @@ namespace BSOA.Column
             }
         }
 
+        private void Init()
+        {
+            if (IsNull == null)
+            {
+                IsNull = new BooleanColumn(NullByDefault);
+            }
+        }
+
         public override int Count => Values.Count;
 
         public override T this[int index]
         {
             get
             {
-                return (IsNull[index] ? null : Values[index]);
+                return ((IsNull?[index] ?? NullByDefault) ? null : Values[index]);
             }
 
             set
             {
+                Init();
                 IsNull[index] = (value == null);
                 Values[index] = value;
             }
@@ -59,13 +70,13 @@ namespace BSOA.Column
 
         public override void Clear()
         {
-            IsNull.Clear();
+            IsNull = null;
             Values.Clear();
         }
 
         public override void Swap(int index1, int index2)
         {
-            IsNull.Swap(index1, index2);
+            IsNull?.Swap(index1, index2);
             Values.Swap(index1, index2);
         }
 
@@ -73,10 +84,13 @@ namespace BSOA.Column
         {
             Values.RemoveFromEnd(count);
 
-            int isNullToRemove = IsNull.Count - Values.Count;
-            if (isNullToRemove > 0)
+            if (IsNull != null)
             {
-                IsNull.RemoveFromEnd(isNullToRemove);
+                int isNullToRemove = IsNull.Count - Values.Count;
+                if (isNullToRemove > 0)
+                {
+                    IsNull.RemoveFromEnd(isNullToRemove);
+                }
             }
         }
 
@@ -87,24 +101,27 @@ namespace BSOA.Column
 
         private static Dictionary<string, Setter<NullableColumn<T>>> setters = new Dictionary<string, Setter<NullableColumn<T>>>()
         {
-            [Names.IsNull] = (r, me) => me.IsNull.Read(r),
-            [Names.Values] = (r, me) => me.Values.Read(r)
+            [Names.IsNull] = (r, me) => { me.Init(); me.IsNull.Read(r); },
+            [Names.Values] = (r, me) => { me.Init(); me.Values.Read(r); }
         };
 
         public void Read(ITreeReader reader)
         {
             reader.ReadObject(this, setters);
 
-            if (IsNull.Count == 0 && Values.Count > 0)
+            if (IsNull != null)
             {
-                // Only wrote values means all values are non-null
-                IsNull[Values.Count - 1] = false;
-                IsNull.SetAll(false);
-            }
-            else if (IsNull.Count > 0 && Values.Count == 0)
-            {
-                // Only wrote nulls means all values are null
-                Values[IsNull.Count - 1] = default(T);
+                if (IsNull.Count == 0 && Values.Count > 0)
+                {
+                    // Only wrote values means all values are non-null
+                    IsNull[Values.Count - 1] = false;
+                    IsNull.SetAll(false);
+                }
+                else if (IsNull.Count > 0 && Values.Count == 0)
+                {
+                    // Only wrote nulls means all values are null
+                    Values[IsNull.Count - 1] = default(T);
+                }
             }
         }
 
@@ -114,7 +131,7 @@ namespace BSOA.Column
 
             if (Count > 0)
             {
-                int nullValueCount = IsNull.CountTrue;
+                int nullValueCount = IsNull?.CountTrue ?? (NullByDefault ? Count : 0);
 
                 if (nullValueCount == Count)
                 {
